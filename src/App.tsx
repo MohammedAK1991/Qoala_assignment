25ea793eb1df218d3aace4e68e8a772138800618
Mohammed <mohmed.ak.17@gmail.com>
2020-09-27 02:55:15 +0200

    feat(App) : add usEffect hook to fetch data when component mounts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,53 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import unsplash from './api/unsplash';
+import shuffle from "lodash/shuffle";
 
-function App() {
+const App: React.FC = () => {
+
+  let collectionNames: string[] = ['kids', 'food', 'travel', 'dogs', 'beer', 'guitar', 'books', 'video games'];
+  collectionNames = shuffle(collectionNames);
+
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    unsplash
+      .get('/search/collections', {
+        params: {
+          query: collectionNames.pop().toString(),
+          per_page: 6//eslint-disable-line
+        },
+      })
+      .then((res) => {
+        setImages(res.data.results);
+      })
+      .then(() => {
+        unsplash
+          .get('/search/collections', {
+            params: {
+              query: collectionNames.pop().toString(),
+              per_page: 6//eslint-disable-line
+            },
+          })
+          .then((res) => {
+            setImages(images => [...images, ...res.data.results]);
+          })
+      })
+      .then(() => {
+        unsplash
+          .get('/search/collections', {
+            params: {
+              query: collectionNames.pop().toString(),
+              per_page: 6 //eslint-disable-line
+            },
+          })
+          .then((res) => {
+            setIsLoading(false);
+            setImages(images => [...images, ...res.data.results]);
+          })
+      })
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello</h1>
