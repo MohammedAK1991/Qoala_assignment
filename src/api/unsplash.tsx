f35c5bc9e42a2920e392a5ec3cb0026df170de22
Mohammed <mohmed.ak.17@gmail.com>
2020-09-27 02:39:34 +0200

    chore(api) : make .env file move api key to .env, make .env.example file

--- src/api/unsplash.tsx
+++ src/api/unsplash.tsx
@@ -4,6 +4,6 @@ export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
         Authorization:
-          'Client-ID dS5eABsdXxYSaKvrkWUQ0M--dLPJxjv_QQOz1B6_zfA'
+          `Client-ID ${process.env.REACT_APP_UNSPLASH_API_ACCESS_KEY}`
     }
 });
