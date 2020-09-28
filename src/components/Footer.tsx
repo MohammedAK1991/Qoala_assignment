e90a54fb871f83490135c97350a75790b28c68e2
Mohammed <mohmed.ak.17@gmail.com>
2020-09-28 15:58:15 +0200

    style(all) : fix styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,16 +16,16 @@ export default function Footer() {
         <h4 style={{color:'black'}}>Follow Me!</h4>
         <List horizontal divided link size='large'>
           <List.Item as='a' href='https://www.instagram.com/mohmedak_/' target="_blank">
-            <i className="instagram blue icon large"></i>
+            <i className="instagram icon big"></i>
           </List.Item>
           <List.Item as='a' href='https://www.linkedin.com/in/mohmedak/' target="_blank">
-            <i className="linkedin blue icon large"></i>
+            <i className="linkedin icon big"></i>
           </List.Item>
           <List.Item as='a' href='https://github.com/MohammedAK1991' target="_blank">
-            <i className="github icon large"></i>
+            <i className="github icon big"></i>
           </List.Item>
           <List.Item as='a' href='https://mohammedak1991.github.io/mohammedak.github.io/' target="_blank">
-            <i className="user secret icon large"></i>
+            <i className="user secret icon big"></i>
           </List.Item>
         </List>
       </Container>
