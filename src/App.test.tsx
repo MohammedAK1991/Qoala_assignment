bdb6b986457eb6eaafbbfb972dade566e9fecc7d
Mohammed <mohmed.ak.17@gmail.com>
2020-09-29 01:56:17 +0200

    test(App) : add basic unit tests for App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import App from './App';
+
+// test('renders title', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('renders the App without crashing', () => {
+  const div = document.createElement('div');
+  debugger;
+  ReactDOM.render(<App />, div);
+});
+
+it('renders main title', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(getByText('Qoala Assignment')).toBeInTheDocument();
 });
