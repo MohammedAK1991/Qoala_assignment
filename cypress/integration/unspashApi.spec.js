6c09277be2fd043def01d91892131dce9d521fc1
Mohammed <mohmed.ak.17@gmail.com>
2020-09-29 02:06:51 +0200

    test(API) : check if unsplash API is working correctly

---
+++ cypress/integration/unspashApi.spec.js
@@ -0,0 +1,23 @@
+describe('Request', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+  it('displays images fetched from unsplash API', () => {
+    cy.request('https://api.unsplash.com', {
+      params: {
+        query: 'nature',
+        per_page: 6
+      },
+      headers: {
+        Authorization:
+          `Client-ID ${process.env.REACT_APP_UNSPLASH_API_ACCESS_KEY}`
+      }
+    })
+      .should((response) => {
+        expect(response.status).to.eq(200)
+        // expect(response.body).to.have.length(15)
+        // expect(response).to.have.property('headers')
+        // expect(response).to.have.property('duration')
+      })
+  })
+})
