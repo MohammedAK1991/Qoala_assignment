39f2c27f837e6501e4c311eeb494aa5e74da5d10
Mohammed <mohmed.ak.17@gmail.com>
2020-09-29 03:02:47 +0200

    style(Main) : Make navbar stackable instead of flex shrink

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react'
-import { Input, Menu, Segment, Sticky } from 'semantic-ui-react';
+import { Input, Menu, Segment, Sticky, Image } from 'semantic-ui-react';
 import ImageList from './ImageList';
 
 type MainProps = {
@@ -22,13 +22,14 @@ const Main: React.FC <MainProps> = ({ images, onSubmit, isLoading }) => {
     <div ref={contextRef} style={{ fontFamily: 'Avenir' }}>
       <Sticky context={contextRef}>
         <Menu
+          stackable
           attached='top'
           tabular
           style={{ backgroundColor: '#83c5be', paddingTop: '0em' }}
         >
           <Menu.Item as='a' href="/">
-            <img src="/qoala.svg" alt="logo" />
-            <span style={{ fontFamily: 'Avenir', marginLeft: '5px', fontSize: '22px', fontWeight:'bolder' }}>
+            <img src="/qoala.svg" alt="logo" style={{width:'50px'}} />
+            <span style={{ fontFamily: 'Avenir', marginLeft: '1.4vw', fontSize: '24px', fontWeight: 800, marginBottom:0 }}>
               Qoala Assignment
             </span>
           </Menu.Item>
@@ -36,13 +37,13 @@ const Main: React.FC <MainProps> = ({ images, onSubmit, isLoading }) => {
             <Menu.Item>
               <form onSubmit={onFormSubmit} >
                 <Input
-                  fluid
                   autoFocus
                   id="search"
-                  action={{ icon: 'search', color:'orange' }}
-                  style={{ maxWidth: '30vw', minWidth: '22vw'}}
+                  action={{ icon: 'search', color:'orange', circular : true }}
+                  style={{ maxWidth: '320px', minWidth: '320px'}}
                   value={term}
                   onChange={(event) => setTerm(event.target.value)}
+                  placeholder="Search for collection..."
                 />
               </form>
             </Menu.Item>
